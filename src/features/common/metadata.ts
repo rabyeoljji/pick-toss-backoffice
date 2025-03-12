a2090f6103020c849d201dfc957a8d48f2fe9819
rabyeoljji <nageuna922@gmail.com>
2025-03-12 03:13:03 +0000

    feat: favicons

--- src/features/common/metadata.ts
+++ src/features/common/metadata.ts
@@ -1,14 +1,18 @@
 import { Metadata } from 'next'
 
+// url 변경되면 수정 부탁드립니다
 export const Metadatas = {
   root(): Metadata {
+    const baseUrl = 'https://pick-toss-backoffice.vercel.app/'
+
     return {
-      // metadataBase: new URL(`https://www.picktoss.com`),
+      metadataBase: new URL(baseUrl),
       title: '픽토스 백오피스',
       description: 'picktoss BackOffice',
-      // icons: {
-      //   icon: '/images/picktoss-logo.png',
-      // },
+      icons: {
+        icon: '/favicons/favicon-96x96.png',
+        apple: '/favicons/apple-touch-icon.png',
+      },
     }
   },
 }
